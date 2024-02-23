87b6ea8d3b49c3333fc5e7dde04021daac0ae114
Juan Aguenza <juan.aguenza@gmail.com>
2024-02-23 12:25:28 -0800

    updated macOS

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
@@ -43,5 +60,8 @@ return {
     },
 
     -- see below for full list of options 👇
+  },
+  {
+    "xiyaowong/transparent.nvim",
   },
 }
