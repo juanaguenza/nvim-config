c784209846ec637bcf796ef97f984b0babff4252
Juan Aguenza <juan.aguenza@gmail.com>
2024-04-08 00:59:57 -0700

    Updated my config - added a plugin to do leetcode within nvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -136,4 +136,25 @@ return {
   {
     "xiyaowong/transparent.nvim",
   },
+  {
+    {
+      "kawre/leetcode.nvim",
+      build = ":TSUpdate html",
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+      },
+      opts = {
+        -- configuration goes here
+        ---@type lc.lang
+        lang = "python",
+      },
+    },
+  },
 }
